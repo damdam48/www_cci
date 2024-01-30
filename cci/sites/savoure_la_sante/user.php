8ef5c95110aef33633fc84b84f5eb406b1329f46
damdam48 <damdam.formation@gmail.com>
2024-01-30 09:45:50 +0100

    user.php, image dans la BD, mais il reste des chose a faire

--- cci/sites/savoure_la_sante/user.php
+++ cci/sites/savoure_la_sante/user.php
@@ -6,6 +6,8 @@ page user_id <?php echo $_GET['user_id'];?><hr>
 
         if (isset($_POST['update'])) {
             // print_r($_POST);
+            // print_r($_FILES);
+            // echo $_FILES['avatar']['name'];
      // UPDATE
             try {
                 $sql = "UPDATE users SET name = ?, first_name = ?, mail = ?, pass = ?, img = ? WHERE user_id = ? ";
@@ -16,7 +18,7 @@ page user_id <?php echo $_GET['user_id'];?><hr>
                         strip_tags($_POST['first_name']),
                         strip_tags($_POST['mail']),
                         strip_tags($_POST['pass']),
-                        // $_FILES['avatar']['name'],
+                        $_FILES['avatar']['name'],
                         $_GET['user_id']
                     )
                 );
@@ -26,7 +28,7 @@ page user_id <?php echo $_GET['user_id'];?><hr>
 
             //si image envoyer
             if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] == 0 ) {
-                $moveFiles = true;
+                $uploadImgOk = true;
 
                     // retrieve image info
                         //size Ko sizeMo, sizeMax
@@ -46,7 +48,6 @@ page user_id <?php echo $_GET['user_id'];?><hr>
                         } else {
                             echo 'OK';
                         }
-
 
                         $extension = $_FILES['avatar']['type'];
                         // Séparer les termes
@@ -84,22 +85,10 @@ page user_id <?php echo $_GET['user_id'];?><hr>
 
 
 
-                        // extension
 
-                    // test size
-                        // si no good
-                            // $moveFile = false;
-
-                    // test extension
-                        //si no good
-                            // $moveFile = false;
-
-                    // si $moveFile == true
-                        // move_uploaded_file(filename, destination)
 
 
             }
-            // 
 
      // fin UPDATE
         }
@@ -123,14 +112,12 @@ page user_id <?php echo $_GET['user_id'];?><hr>
 
             echo 'nous n\'avons pas trouvé cet utilisateur. <br><a href="index.php?p=users.php"> Revenir a la page utilisateurs</a>';
         }
-        // else {
-        //     echo 'OK trouvé';
-        // }
+
     // end REQUEST select
         
 ?>
 
-<form method="POST" class="contener border border border-primary rounded mx-5">
+<form method="POST" enctype="multipart/form-data" class="contener border border border-primary rounded mx-5">
 
     <div class="form-group row">
         <label for="name" class="col-sm-2 col-form-label w-auto mx-auto">name</label>
@@ -164,93 +151,8 @@ page user_id <?php echo $_GET['user_id'];?><hr>
     <input type="submit" value="update" name="update" class="btn btn-primary">
     <br>
     <input type="file" name="avatar">
+
     <input type="submit" name="upload">
 
 
 </form>
-
-
-<!-- <h2>Changer votre avatar</h2>
-
-<?php
-if (isset($_POST['upload']) && $_FILES['avatar']['error'] == 0) {
-
-    $uploadImgOk = true;
-
-    // modifier taille max des fichiers uploadés
-        // Icone wamp > Clic gauche
-            // Php.ini
-                // Chercher "upload_max_filesize"
-                    // Modifier taille
-                        // Redemarrer wamp
-    print_r($_POST);
-    echo '<hr>';
-    print_r($_FILES);
-    echo '<hr>';
-    // Montrer taille image
-    echo 'poids de l\'image : ' . $_FILES['avatar']['size'] . '<br>';
-    // Montrer type image
-    echo 'type de l\'image : ' . $_FILES['avatar']['type'] . '<br>';
-    echo '<br>';
-
-
-    // Test image size
-    $sizeKo = $_FILES['avatar']['size'];
-    echo ' sizeKo ' . $sizeKo . '<br>';
-
-    $sizeMo = $sizeKo / 1000000;
-    echo ' sizeMo ' . $sizeMo . '<br>';
-
-    $sizeMaxMo = 3;
-
-    echo 'Votre image pèse ' . $sizeMo . ' Mo<br>';
-
-    if ($sizeMo > $sizeMaxMo) {
-        echo 'Le fichier est trop volumineux.<br>';
-        $uploadImgOk = false;
-    } else {
-        echo 'OK';
-    }
-    // Separer le type de l'image pour récuperer seulement le jpeg sur image/jpeg par exemple
-    // Extention
-    $extension = $_FILES['avatar']['type'];
-    // Séparer les termes
-    $array = explode('/', $extension);
-    print_r($array);
-    $extension = $array[1];
-    echo '<br>extension ' . $extension . '<br>';
-
-    $extensionAllowed = ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'svg', 'webp','pdf'];
-    print_r($extensionAllowed);
-
-    // test extension dans les extensions autorisées
-    if (in_array($extension, $extensionAllowed)) {
-        echo 'extension OK<br>';
-    }
-    else {
-        echo 'extension NO<br>';
-        $uploadImgOk = false;
-    }
-
-    if ($uploadImgOk == true) {
-        $folder = 'img/users/';
-        $newName = 'image_1';
-
-        if (@move_uploaded_file($_FILES['avatar']['tmp_name'] , $folder . $newName . '.' .$extension )) {
-            echo 'upload ok';
-        }
-        else {
-            echo 'upload NO';
-        }
-    }
-    else {
-        echo 'L\'image n\'a pas pu être envoyé';
-    }
-}
-
-?> -->
-
-<!-- <form method="post" enctype="multipart/form-data">
-    <input type="file" name="avatar">
-    <input type="submit" name="upload">
-</form> -->
