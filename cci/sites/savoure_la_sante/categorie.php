915e587a6c729316c3caf22b8b98998cb8a4ba2c
damdam48 <damdam.formation@gmail.com>
2024-01-31 15:18:38 +0100

    fin de mise a jour de savoure_la_sante

--- cci/sites/savoure_la_sante/categorie.php
+++ cci/sites/savoure_la_sante/categorie.php
@@ -1,136 +1,138 @@
 <h1>categorie</h1>
-        
-categorieid <?php echo $_GET['categorie_id'];?><hr>
+
+categorieid
+<?php echo $_GET['categorie_id']; ?>
+<hr>
 
 <?php
 
-        if (isset($_POST['update'])) {
-            // print_r($_POST);
-            // echo '<br>';
-            // print_r($_FILES);
-            // echo '<br>';
-            // echo $_FILES['avatar']['name'];
-     // UPDATE
-            try {
-                $sql = "UPDATE categorie SET name = ?, categorie_cat = ?, dateUpdate = ? WHERE categorie_id = ? ";
-                $stmt = $bdd->prepare($sql);
-                $stmt->execute(
-                    array(
-                        strip_tags($_POST['name']),
-                        strip_tags($_POST['categorie_cat']),
-                        date('Y-m-d H:i:s'),
-                        $_GET['categorie_id']
-                    )
-                );
-            } catch (Exception $e) {
-                print "Erreur ! " . $e->getMessage() . "<br/>";
-            }
-
-            //si image envoyer
-            if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] == 0 ) {
-                $uploadImgOk = true;
-                        // print_r($_FILES);
-
-                    // retrieve image info
-                        //size Ko sizeMo, sizeMax
-                        $sizeKo = $_FILES['avatar']['size'];
-                            // echo ' sizeKo ' . $sizeKo . '<br>';
-
-                        $sizeMo = $sizeKo / 1000000;
-                            // echo ' sizeMo ' . $sizeMo . '<br>';
-
-                        $sizeMaxMo = 3;
-                            // echo 'Votre image pèse ' . $sizeMo . ' Mo<br>';
-
-                        if ($sizeMo > $sizeMaxMo) {
-                        echo 'Le fichier est trop volumineux.<br>';
-                        $uploadImgOk = false;
-                        } else {
-                            // echo 'OK';
-                        }
-
-                        $extension = $_FILES['avatar']['type'];
-                        // Séparer les termes
-                        $array = explode('/', $extension);
-                            // print_r($array);
-
-                        $extension = $array[1];
-                            // echo '<br>extension ' . $extension . '<br>';
-                    
-                        $extensionAllowed = ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'svg', 'webp','pdf'];
-                            // print_r($extensionAllowed);
-                    
-                        // test extension dans les extensions autorisées
-                        if (in_array($extension, $extensionAllowed)) {
-                                // echo 'extension OK<br>';
-                        }
-                        else {
-                            echo 'extension NO<br>';
-                            $uploadImgOk = false;
-                        }
-                    
-                        if ($uploadImgOk == true) {
-                            $folder = 'img/categorie/';
-                            $newName = 'image_1';
-                    
-
-                            // if (@move_uploaded_file($_FILES['avatar']['tmp_name'] , $folder . $newName . '.' .$extension )) {
-                                if (@move_uploaded_file(str_replace(" ", "-", $_FILES['avatar']['tmp_name']) , $folder . str_replace(" ", "-", $_FILES['avatar']['name']))) {
-                                
-                                    // echo 'upload ok';
-
-                                // update table img
-                                try {
-                                    $sql = "UPDATE categorie SET img = ? WHERE categorie_id = ? ";
-                                    $stmt = $bdd->prepare($sql);
-                                    $stmt->execute(
-                                        array(
-                                            // $newName.'.'.$extension,
-                                            // $_FILES['avatar']['name'],
-                                            str_replace(" ", "-", $_FILES['avatar']['name']),
-                                            $_GET['categorie_id']
-                                        )
-                                    );
-                                } catch (Exception $e) {
-                                    print "Erreur ! " . $e->getMessage() . "<br/>";
-                                }
-                            }
-                            else {
-                                echo 'upload NO';
-                            }
-                        }
-                        else {
-                            echo 'L\'image n\'a pas pu être envoyé';
-                        }
-            }
-
-     // fin UPDATE
-        }
-
-
-    // REQUEST select
-        try { 
-            $sql="SELECT * FROM categorie WHERE categorie_id = ?";
-            $stmt = $bdd->prepare($sql);
-            $stmt->execute( array(
+if (isset($_POST['update'])) {
+    // print_r($_POST);
+    // echo '<br>';
+    // print_r($_FILES);
+    // echo '<br>';
+    // echo $_FILES['avatar']['name'];
+    // UPDATE
+    try {
+        $sql = "UPDATE categorie SET name = ?, categorie_cat = ?, dateUpdate = ? WHERE categorie_id = ? ";
+        $stmt = $bdd->prepare($sql);
+        $stmt->execute(
+            array(
+                strip_tags($_POST['name']),
+                strip_tags($_POST['categorie_cat']),
+                date('Y-m-d H:i:s'),
                 $_GET['categorie_id']
-            ) );
-            } catch (Exception $e) {print "Erreur ! " . $e->getMessage() . "<br/>";}
-        
-        // construct results
-        $results=$stmt->fetch(PDO::FETCH_ASSOC);
-            // print_r($results);
-            // echo '<br>';
+            )
+        );
+    } catch (Exception $e) {
+        print "Erreur ! " . $e->getMessage() . "<br/>";
+    }
 
-        if (empty($results)) {
+    //si image envoyer
+    if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] == 0) {
+        $uploadImgOk = true;
+        // print_r($_FILES);
 
-            // echo 'nous n\'avons pas trouvé cet utilisateur. <br><a href="index.php?p=categories.php"> Revenir a la page utilisateurs</a>';
+        // retrieve image info
+        //size Ko sizeMo, sizeMax
+        $sizeKo = $_FILES['avatar']['size'];
+        // echo ' sizeKo ' . $sizeKo . '<br>';
+
+        $sizeMo = $sizeKo / 1000000;
+        // echo ' sizeMo ' . $sizeMo . '<br>';
+
+        $sizeMaxMo = 3;
+        // echo 'Votre image pèse ' . $sizeMo . ' Mo<br>';
+
+        if ($sizeMo > $sizeMaxMo) {
+            echo 'Le fichier est trop volumineux.<br>';
+            $uploadImgOk = false;
+        } else {
+            // echo 'OK';
         }
 
+        $extension = $_FILES['avatar']['type'];
+        // Séparer les termes
+        $array = explode('/', $extension);
+        // print_r($array);
+
+        $extension = $array[1];
+        // echo '<br>extension ' . $extension . '<br>';
+
+        $extensionAllowed = ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'svg', 'webp', 'pdf'];
+        // print_r($extensionAllowed);
+
+        // test extension dans les extensions autorisées
+        if (in_array($extension, $extensionAllowed)) {
+            // echo 'extension OK<br>';
+        } else {
+            echo 'extension NO<br>';
+            $uploadImgOk = false;
+        }
+
+        if ($uploadImgOk == true) {
+            $folder = 'img/categorie/';
+            $newName = 'image_1';
 
 
-    // end REQUEST select
-        
+            // if (@move_uploaded_file($_FILES['avatar']['tmp_name'] , $folder . $newName . '.' .$extension )) {
+            if (@move_uploaded_file(str_replace(" ", "-", $_FILES['avatar']['tmp_name']), $folder . str_replace(" ", "-", $_FILES['avatar']['name']))) {
+
+                // echo 'upload ok';
+
+                // update table img
+                try {
+                    $sql = "UPDATE categorie SET img = ? WHERE categorie_id = ? ";
+                    $stmt = $bdd->prepare($sql);
+                    $stmt->execute(
+                        array(
+                            // $newName.'.'.$extension,
+                            // $_FILES['avatar']['name'],
+                            str_replace(" ", "-", $_FILES['avatar']['name']),
+                            $_GET['categorie_id']
+                        )
+                    );
+                } catch (Exception $e) {
+                    print "Erreur ! " . $e->getMessage() . "<br/>";
+                }
+            } else {
+                echo 'upload NO';
+            }
+        } else {
+            echo 'L\'image n\'a pas pu être envoyé';
+        }
+    }
+
+    // fin UPDATE
+}
+
+
+// REQUEST select
+try {
+    $sql = "SELECT * FROM categorie WHERE categorie_id = ?";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute(
+        array(
+            $_GET['categorie_id']
+        ));
+} catch (Exception $e) {
+    print "Erreur ! " . $e->getMessage() . "<br/>";
+}
+
+// construct results
+$results = $stmt->fetch(PDO::FETCH_ASSOC);
+// print_r($results);
+// echo '<br>';
+
+if (empty($results)) {
+
+    // echo 'nous n\'avons pas trouvé cet utilisateur. <br><a href="index.php?p=categories.php"> Revenir a la page utilisateurs</a>';
+}
+
+
+
+// end REQUEST select
+
 ?>
 
 
@@ -140,72 +142,40 @@ categorieid <?php echo $_GET['categorie_id'];?><hr>
     <div class="form-group row">
         <label for="name" class="col-sm-2 col-form-label w-auto mx-auto">name</label>
         <div class="col-sm-10">
-            <input  type="text" name="name" value="<?php echo $results['name']; ?>" class="form-control" id="staticEmail" placeholder="name"></input>
+            <input type="text" name="name" value="<?php echo $results['name']; ?>" class="form-control" id="staticEmail"
+                placeholder="name"></input>
         </div>
     </div>
-
-
-    <!-- <div class="form-group row">
-        <label for="first_name" class="col-sm-2 col-form-label w-auto mx-auto">first_name</label>
-        <div class="col-sm-10">
-            <input type="text" name="first_name" value="<?php echo $results['first_name'] ?>" class="form-control" placeholder="first_name"></input>
-    </div>
-    </div> -->
-
-    <!-- <div class="form-group row">
-        <label for="mail" class="col-sm-2 col-form-label w-auto mx-auto">mail</label>
-        <div class="col-sm-10">
-            <input type="mail" name="mail" value="<?php echo $results['mail']; ?>" class="form-control" placeholder="mail"></input>
-        </div>
-    </div> -->
-
-    <!-- <div class="form-group row">
-        <label for="pass" class="col-sm-2 col-form-label w-auto mx-auto">password</label>
-        <div class="col-sm-10">
-            <input type="password" name="pass" value="<?php echo $results['pass']; ?>" class="form-control" placeholder="password"></input>
-        </div>
-    </div> -->
 
     <div class="form-group row col-sm-2 col-form-label mx-auto ">
         <input type="file" name="avatar">
-        <!-- <input type="submit" name="upload"> -->
-
-
 
     </div>
+
+
+    <?php
+
+    // categorie_cat 
+    try {
+        $sql = "SELECT * FROM categorie_cat";
+        $stmt_cats = $bdd->prepare($sql);
+        $stmt_cats->execute();
+    } catch (Exception $e) {
+        print "Erreur ! " . $e->getMessage() . "<br/>";
+    }
+
+    ?>
+    <select name="categorie_cat" class="form-control w-auto mx-auto text-center">
+        <?php while ($categories = $stmt_cats->fetch(PDO::FETCH_ASSOC)) {
+            // print_r($categories);
+            echo '<option value="' . $categories['categorie_cat_id'] . '"';
+            echo $categories['categorie_cat_id'] == $results['categorie_cat'] ? ' selected' : '';
+            echo '>' . $categories['categorie_cat_name'] . '</option>';
+        } ?>
+    </select>
 
     <label for=""></label>
     <input type="submit" value="update" name="update" class="btn btn-primary">
     <br>
 
-
-
-
-    <?php
-
-        // categorie_cat 
-            try { 
-                $sql="SELECT * FROM categorie_cat";
-                $stmt_cats = $bdd->prepare($sql);
-                $stmt_cats->execute();
-                } catch (Exception $e) {print "Erreur ! " . $e->getMessage() . "<br/>";}
-
-            ?>
-            <select name="categorie_cat" class="form-control w-auto mx-auto text-center">
-                <?php while ($categories=$stmt_cats->fetch(PDO::FETCH_ASSOC)) {
-                    // print_r($categories);
-                    echo '<option value="'.$categories['categorie_cat_id'].'"'; 
-                    echo $categories['categorie_cat_id']==$results['categorie_cat']?' selected':'';
-                    echo '>' . $categories['categorie_cat_name'].'</option>';
-                } ?>
-            </select>
-
-
-
-        
 </form>
-
-
-
-
-
--- cci/sites/savoure_la_sante/user.php
+++ cci/sites/savoure_la_sante/user.php
@@ -1,133 +1,135 @@
 <h1>user</h1>
-        
-user_id <?php echo $_GET['user_id'];?><hr>
+
+user_id
+<?php echo $_GET['user_id']; ?>
+<hr>
 
 <?php
 
-        if (isset($_POST['update'])) {
-            // print_r($_POST);
-            // print_r($_FILES);
-            // echo $_FILES['avatar']['name'];
-     // UPDATE
-            try {
-                $sql = "UPDATE users SET name = ?, first_name = ?, mail = ?, pass = ? WHERE user_id = ? ";
-                $stmt = $bdd->prepare($sql);
-                $stmt->execute(
-                    array(
-                        strip_tags($_POST['name']),
-                        strip_tags($_POST['first_name']),
-                        strip_tags($_POST['mail']),
-                        strip_tags($_POST['pass']),
-                        $_GET['user_id']
-                    )
-                );
-            } catch (Exception $e) {
-                print "Erreur ! " . $e->getMessage() . "<br/>";
-            }
-
-            //si image envoyer
-            if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] == 0 ) {
-                $uploadImgOk = true;
-                        // print_r($_FILES);
-
-                    // retrieve image info
-                        //size Ko sizeMo, sizeMax
-                        $sizeKo = $_FILES['avatar']['size'];
-                            // echo ' sizeKo ' . $sizeKo . '<br>';
-
-                        $sizeMo = $sizeKo / 1000000;
-                            // echo ' sizeMo ' . $sizeMo . '<br>';
-
-                        $sizeMaxMo = 3;
-                            // echo 'Votre image pèse ' . $sizeMo . ' Mo<br>';
-
-                        if ($sizeMo > $sizeMaxMo) {
-                        echo 'Le fichier est trop volumineux.<br>';
-                        $uploadImgOk = false;
-                        } else {
-                            // echo 'OK';
-                        }
-
-                        $extension = $_FILES['avatar']['type'];
-                        // Séparer les termes
-                        $array = explode('/', $extension);
-                            // print_r($array);
-
-                        $extension = $array[1];
-                            // echo '<br>extension ' . $extension . '<br>';
-                    
-                        $extensionAllowed = ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'svg', 'webp','pdf'];
-                            // print_r($extensionAllowed);
-                    
-                        // test extension dans les extensions autorisées
-                        if (in_array($extension, $extensionAllowed)) {
-                                // echo 'extension OK<br>';
-                        }
-                        else {
-                            echo 'extension NO<br>';
-                            $uploadImgOk = false;
-                        }
-                    
-                        if ($uploadImgOk == true) {
-                            $folder = 'img/users/';
-                            $newName = 'image_1';
-                    
-
-                            // if (@move_uploaded_file($_FILES['avatar']['tmp_name'] , $folder . $newName . '.' .$extension )) {
-                                if (@move_uploaded_file(str_replace(" ", "-", $_FILES['avatar']['tmp_name']) , $folder . str_replace(" ", "-", $_FILES['avatar']['name']))) {
-                                
-                                    // echo 'upload ok';
-
-                                // update table img
-                                try {
-                                    $sql = "UPDATE users SET img = ? WHERE user_id = ? ";
-                                    $stmt = $bdd->prepare($sql);
-                                    $stmt->execute(
-                                        array(
-                                            // $newName.'.'.$extension,
-                                            // $_FILES['avatar']['name'],
-                                            str_replace(" ", "-", $_FILES['avatar']['name']),
-                                            $_GET['user_id']
-                                        )
-                                    );
-                                } catch (Exception $e) {
-                                    print "Erreur ! " . $e->getMessage() . "<br/>";
-                                }
-                            }
-                            else {
-                                echo 'upload NO';
-                            }
-                        }
-                        else {
-                            echo 'L\'image n\'a pas pu être envoyé';
-                        }
-            }
-
-     // fin UPDATE
-        }
-
-
-    // REQUEST select
-        try { 
-            $sql="SELECT * FROM users WHERE user_id = ?";
-            $stmt = $bdd->prepare($sql);
-            $stmt->execute( array(
+if (isset($_POST['update'])) {
+    // print_r($_POST);
+    // print_r($_FILES);
+    // echo $_FILES['avatar']['name'];
+    // UPDATE
+    try {
+        $sql = "UPDATE users SET name = ?, first_name = ?, mail = ?, pass = ? WHERE user_id = ? ";
+        $stmt = $bdd->prepare($sql);
+        $stmt->execute(
+            array(
+                strip_tags($_POST['name']),
+                strip_tags($_POST['first_name']),
+                strip_tags($_POST['mail']),
+                strip_tags($_POST['pass']),
                 $_GET['user_id']
-            ) );
-            } catch (Exception $e) {print "Erreur ! " . $e->getMessage() . "<br/>";}
-        
-        // construct results
-        $results=$stmt->fetch(PDO::FETCH_ASSOC);
-            // print_r($results);
-            // echo '<br>';
+            )
+        );
+    } catch (Exception $e) {
+        print "Erreur ! " . $e->getMessage() . "<br/>";
+    }
 
-        if (empty($results)) {
+    //si image envoyer
+    if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] == 0) {
+        $uploadImgOk = true;
+        // print_r($_FILES);
 
-            echo 'nous n\'avons pas trouvé cet utilisateur. <br><a href="index.php?p=users.php"> Revenir a la page utilisateurs</a>';
+        // retrieve image info
+        //size Ko sizeMo, sizeMax
+        $sizeKo = $_FILES['avatar']['size'];
+        // echo ' sizeKo ' . $sizeKo . '<br>';
+
+        $sizeMo = $sizeKo / 1000000;
+        // echo ' sizeMo ' . $sizeMo . '<br>';
+
+        $sizeMaxMo = 3;
+        // echo 'Votre image pèse ' . $sizeMo . ' Mo<br>';
+
+        if ($sizeMo > $sizeMaxMo) {
+            echo 'Le fichier est trop volumineux.<br>';
+            $uploadImgOk = false;
+        } else {
+            // echo 'OK';
         }
 
-    // end REQUEST select
-        
+        $extension = $_FILES['avatar']['type'];
+        // Séparer les termes
+        $array = explode('/', $extension);
+        // print_r($array);
+
+        $extension = $array[1];
+        // echo '<br>extension ' . $extension . '<br>';
+
+        $extensionAllowed = ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'svg', 'webp', 'pdf'];
+        // print_r($extensionAllowed);
+
+        // test extension dans les extensions autorisées
+        if (in_array($extension, $extensionAllowed)) {
+            // echo 'extension OK<br>';
+        } else {
+            echo 'extension NO<br>';
+            $uploadImgOk = false;
+        }
+
+        if ($uploadImgOk == true) {
+            $folder = 'img/users/';
+            $newName = 'image_1';
+
+
+            // if (@move_uploaded_file($_FILES['avatar']['tmp_name'] , $folder . $newName . '.' .$extension )) {
+            if (@move_uploaded_file(str_replace(" ", "-", $_FILES['avatar']['tmp_name']), $folder . str_replace(" ", "-", $_FILES['avatar']['name']))) {
+
+                // echo 'upload ok';
+
+                // update table img
+                try {
+                    $sql = "UPDATE users SET img = ? WHERE user_id = ? ";
+                    $stmt = $bdd->prepare($sql);
+                    $stmt->execute(
+                        array(
+                            // $newName.'.'.$extension,
+                            // $_FILES['avatar']['name'],
+                            str_replace(" ", "-", $_FILES['avatar']['name']),
+                            $_GET['user_id']
+                        )
+                    );
+                } catch (Exception $e) {
+                    print "Erreur ! " . $e->getMessage() . "<br/>";
+                }
+            } else {
+                echo 'upload NO';
+            }
+        } else {
+            echo 'L\'image n\'a pas pu être envoyé';
+        }
+    }
+
+    // fin UPDATE
+}
+
+
+// REQUEST select
+try {
+    $sql = "SELECT * FROM users WHERE user_id = ?";
+    $stmt = $bdd->prepare($sql);
+    $stmt->execute(
+        array(
+            $_GET['user_id']
+        ));
+} catch (Exception $e) {
+    print "Erreur ! " . $e->getMessage() . "<br/>";
+}
+
+// construct results
+$results = $stmt->fetch(PDO::FETCH_ASSOC);
+// print_r($results);
+// echo '<br>';
+
+if (empty($results)) {
+
+    echo 'nous n\'avons pas trouvé cet utilisateur. <br><a href="index.php?p=users.php"> Revenir a la page utilisateurs</a>';
+}
+
+// end REQUEST select
+
 ?>
 
 <form method="POST" enctype="multipart/form-data" class="contener border border border-primary rounded mx-5">
@@ -135,28 +137,32 @@ user_id <?php echo $_GET['user_id'];?><hr>
     <div class="form-group row">
         <label for="name" class="col-sm-2 col-form-label w-auto mx-auto">name</label>
         <div class="col-sm-10">
-            <input  type="text" name="name" value="<?php echo $results['name']; ?>" class="form-control" id="staticEmail" placeholder="name"></input>
+            <input type="text" name="name" value="<?php echo $results['name']; ?>" class="form-control" id="staticEmail"
+                placeholder="name"></input>
         </div>
     </div>
 
     <div class="form-group row">
         <label for="first_name" class="col-sm-2 col-form-label w-auto mx-auto">first_name</label>
         <div class="col-sm-10">
-            <input type="text" name="first_name" value="<?php echo $results['first_name'] ?>" class="form-control" placeholder="first_name"></input>
-    </div>
+            <input type="text" name="first_name" value="<?php echo $results['first_name'] ?>" class="form-control"
+                placeholder="first_name"></input>
+        </div>
     </div>
 
     <div class="form-group row">
         <label for="mail" class="col-sm-2 col-form-label w-auto mx-auto">mail</label>
         <div class="col-sm-10">
-            <input type="mail" name="mail" value="<?php echo $results['mail']; ?>" class="form-control" placeholder="mail"></input>
+            <input type="mail" name="mail" value="<?php echo $results['mail']; ?>" class="form-control"
+                placeholder="mail"></input>
         </div>
     </div>
 
     <div class="form-group row">
         <label for="pass" class="col-sm-2 col-form-label w-auto mx-auto">password</label>
         <div class="col-sm-10">
-            <input type="password" name="pass" value="<?php echo $results['pass']; ?>" class="form-control" placeholder="password"></input>
+            <input type="password" name="pass" value="<?php echo $results['pass']; ?>" class="form-control"
+                placeholder="password"></input>
         </div>
     </div>
 
@@ -166,6 +172,28 @@ user_id <?php echo $_GET['user_id'];?><hr>
 
 
 
+        <?php
+
+
+        // role 
+        try {
+            $sql = "SELECT * FROM role_cat";
+            $stmt_cats = $bdd->prepare($sql);
+            $stmt_cats->execute();
+        } catch (Exception $e) {
+            print "Erreur ! " . $e->getMessage() . "<br/>";
+        }
+
+        ?>
+
+        <select name="role_cat" class="form-control w-auto mx-auto text-center">
+            <?php while ($users = $stmt_cats->fetch(PDO::FETCH_ASSOC)) {
+                print_r($users);
+                echo '<option value="' . $users['role_id'] . '"';
+                echo $users['role_id'] == $results['role_cat'] ? ' selected' : '';
+                echo '>' . $users['role'] . '</option>';
+            } ?>
+        </select> 
     </div>
 
     <label for=""></label>
@@ -173,5 +201,3 @@ user_id <?php echo $_GET['user_id'];?><hr>
     <br>
 
 </form>
-
-
