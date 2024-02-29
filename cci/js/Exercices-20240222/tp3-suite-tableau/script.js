aa9466c730d0b1bc8548485ec771974a300ca665
damdam48 <damdam.formation@gmail.com>
2024-02-29 11:02:22 +0100

    fin tp js

--- cci/js/Exercices-20240222/tp3-suite-tableau/script.js
+++ cci/js/Exercices-20240222/tp3-suite-tableau/script.js
@@ -1,3 +1,16 @@
+// 4. Ranger un tableau !
+// Réalisez la fonction sortNumbers qui permettra de séparer un tableau envoyé en paramètre (ou
+// argument) en 2 tableaux :
+// arrayInf, avec les nombre inférieurs à 10
+// arraySup avec les autres (supérieurs à 10)
+// Retourner les deux tableaux dans un objet avec pour propriétés inf et sup
+// Lancer la fonction sortNumber avec un tableau contenant plusieurs valeurs, comme celui-ci :[40, 1,
+// 5, 20, 8, 83, 9]
+// Afficher dans la console :
+// l’objet contenant les deux tableaux, puis
+// le tableau des valeurs inférieures à 10 dans la console, puis
+// Le tableau des valeurs supérieures à 10
+
 /**
     * Fonction qui sépare un tableau en deux tableaux, un avec les valeurs inférieures à 10 et l'autre avec les valeurs supérieures à 10 
     *
---
+++ cci/js/Exercices-20240222/tp6/script.js
@@ -0,0 +1,25 @@
+function tp6() {
+    
+    let bibliotheques = [
+        {titre : "yakari", auteur : "plume", pages : "53", lu : false,}
+
+    ]
+
+    function addBook(titre, auteur) {
+        bibliotheques.push(titre["aaaaaaaa"]);
+        bibliotheques.push(auteur["bbbbbb"]);
+        
+    }
+
+    // liste toute la bibliotheques
+    for (let bibliotheque in bibliotheques) {
+        console.log(bibliotheques[bibliotheque]);
+
+    }
+
+
+addBook("aaaaa", "bbbb")
+}
+
+tp6();
+
